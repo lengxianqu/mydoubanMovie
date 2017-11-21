e58e6f84796165a064716485fba97143f56cf980
wb-wzj323553 <wb-wzj323553@alibaba-inc.com>
2017-11-21 21:42:01 +0800

    豆瓣电影项目工程骨架构建

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Cheader></Cheader>
+    <transition name="fade">
+        <keep-alive exclude="moviesDetail">
+            <router-view/>
+        </keep-alive>
+    </transition>
+    
+  </div>
+</template>
+
+<script>
+import Cheader from './components/common/header.vue';
+export default {
+  name: 'app',
+  components:{
+      'header':Cheader
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
